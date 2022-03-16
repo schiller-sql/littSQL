c505f2900f048cbfeff6212e5e85ecf7c47b379c
leartpro <lenni.protte@gmx.de>
2022-03-16 23:03:58 +0100

    TeacherAuth.svelte

--- frontend/src/routes/TeacherAuth.svelte
+++ frontend/src/routes/TeacherAuth.svelte
@@ -2,6 +2,8 @@
     import { Form, TextInput, PasswordInput, Button, ButtonSet } from "carbon-components-svelte";
     import { replace } from "svelte-spa-router";
     export let isLogin;
+    let email:string, password:string, confirmPassword: string;
+    $: disabled = email.length==0 || password.length==0 || isLogin ? false : confirmPassword.length==0;
 
     function changePath() {
         replace(isLogin ? "/teacher-signup" : "/teacher-login");
@@ -12,10 +14,10 @@
 </script>
 
 <Form on:submit={submit}>
-    <TextInput placeholder="Enter email address..." required></TextInput>
-    <PasswordInput placeholder="Enter password..." required></PasswordInput>
+    <TextInput bind:value={email} placeholder="Enter email address..." required></TextInput>
+    <PasswordInput bind:value={password} placeholder="Enter password..." required></PasswordInput>
     {#if !isLogin}
-    <PasswordInput placeholder="Repeat password..." required></PasswordInput>
+    <PasswordInput bind:value={confirmPassword} placeholder="Repeat password..." required></PasswordInput>
     {/if}
     <ButtonSet>
         <Button type="submit">{#if isLogin} Login {:else} Sign up {/if}</Button>
