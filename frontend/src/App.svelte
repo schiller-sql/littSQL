bb11e008e5d7fe3307ce0544ea631746a8e8256d
Lennart Protte <lenni.protte@gmx.de>
2022-03-16 13:27:42 +0100

    init Branch

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -6,18 +6,3 @@
 <main>
 	<Router {routes} />
 </main>
-
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
