01aac3be20944971245dc112e6442bc96864823a
Olevenbaum <88549096+Olevenbaum@users.noreply.github.com>
2022-03-16 12:48:22 +0100

    Added static HTML

--- frontend/src/lib/sites/AssignmentPage.svelte
+++ frontend/src/lib/sites/AssignmentPage.svelte
@@ -5,8 +5,21 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="">
 </head>
 <body>
-    
+    <header>
+        <nav>
+            <img src="" alt="Logo">
+            <h1>littSQL</h1>
+            <button>Go Back</button>
+            <button></button>
+            <button></button>
+            <button></button>
+        </nav>
+    </header>
+    <main>
+        
+    </main>
 </body>
 </html>
--- frontend/src/lib/sites/TeacherStartpage.svelte
+++ frontend/src/lib/sites/TeacherStartpage.svelte
@@ -5,8 +5,24 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="">
 </head>
 <body>
-    
+    <header>
+        <img src="" alt="Logo">
+        <h1>littSQL</h1>
+    </header>
+    <main>
+        <button></button>
+        <form action="">
+            <button></button>
+            <input type="text">
+            <input type="text">
+            <button type="submit"></button>
+        </form>
+    </main>
+    <footer>
+
+    </footer>
 </body>
 </html>
