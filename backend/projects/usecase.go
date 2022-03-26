04936d1b64c019dd1e246a04c70213d7dc14e55f
bit-burger <65500763+bit-burger@users.noreply.github.com>
2022-03-26 18:24:12 +0100

    Add possible todo for later

--- backend/projects/usecase.go
+++ backend/projects/usecase.go
@@ -8,6 +8,8 @@ type Usecase interface {
 	// TODO: Sort by: is not public; name
 	GetProjectsOfTeacher(teacherID int32) (*[]model.ProjectListing, error)
 	NewProject(teacherID int32, name string) (*model.Project, error)
+	// TODO: Maybe add something similar in the future:
+	//CloneProject(teacherID int32, projectID int32, name string) (*model.Project, error)
 	GetProjectDetails(teacherID int32, projectID int32) (*model.Project, error)
 	EditProject(
 		ID int32,
