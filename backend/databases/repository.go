89b071e98ba89857aa4ccb290c5ff175cc15f744
bit-burger <65500763+bit-burger@users.noreply.github.com>
2022-03-29 17:54:23 +0200

    Change db schema

--- backend/databases/repository.go
+++ backend/databases/repository.go
@@ -4,8 +4,8 @@ import "github.com/schiller-sql/littSQL/model"
 
 type Repository interface {
 	GetDatabasesOfTeacher(teacherID int32) (*[]model.DatabaseListing, error)
-	NewDatabase(teacherID int32, name string) (*model.Database, error)
-	GetDatabase(databaseId int32, withData bool) (*model.Database, error)
+	NewDatabase(teacherID int32, name string) (*model.DatabaseTemplate, error)
+	GetDatabase(databaseId int32, withData bool) (*model.DatabaseTemplate, error)
 	EditDatabase(databaseId int32, sql string, imageData []byte) (*model.Database, error)
 	DeleteDatabase(databaseId int32)
 }
--- backend/model/database.go
+++ backend/model/database.go
@@ -1,15 +1,15 @@
 package model
 
-type Database struct {
+type DatabaseTemplate struct {
 	//[ 0] id                                             INT4                 null: false  primary: true   isArray: false  auto: true   col: INT4            len: -1      default: []
 	ID int32 `gorm:"primary_key;AUTO_INCREMENT;column:id;type:INT4;" json:"id"`
 	//[ 1] name                                           VARCHAR              null: false  primary: false  isArray: false  auto: false  col: VARCHAR         len: -1      default: []
 	Name string `gorm:"column:name;type:VARCHAR;" json:"name"`
-	//[ 2] data                                           BYTEA                null: false  primary: false  isArray: false  auto: false  col: BYTEA           len: -1      default: []
-	Data string `gorm:"column:data;type:BYTEA;" json:"data"`
-	//[ 3] schema_svg_path                                VARCHAR              null: false  primary: false  isArray: false  auto: false  col: VARCHAR         len: -1      default: []
-	SchemaSVGPath *string `gorm:"column:schema_svg_path;type:VARCHAR;" json:"schema_svg_path"`
-	//[ 4] owner_id                                        INT4                 null: true   primary: false  isArray: false  auto: false  col: INT4            len: -1      default: []
+	//[ 2] name                                           TEXT null: false  primary: false  isArray: false  auto: false  col: VARCHAR         len: -1      default: []
+	Description string `gorm:"column:description;type:TEXT;" json:"description"`
+	//[ 3] name                                           TEXT null: false  primary: false  isArray: false  auto: false  col: VARCHAR         len: -1      default: []
+	SQL string `gorm:"column:sql;type:TEXT;" json:"sql"`
+	//[ 4] owner_id                                       INT4                 null: true   primary: false  isArray: false  auto: false  col: INT4            len: -1      default: []
 	OwnerID *int32 `gorm:"column:owner_id;type:INT4;" json:"owner_id"`
 }
 
