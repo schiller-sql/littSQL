db9322c287efd7aaf2bee9842796630ec5037b31
bit-burger <65500763+bit-burger@users.noreply.github.com>
2022-04-05 12:24:30 +0200

    make helpers more flexible

--- backend/helpers/get_id.go
+++ backend/helpers/get_id.go
@@ -6,8 +6,12 @@ import (
 	"strconv"
 )
 
-func GetParamID(c *gin.Context) (int32, error) {
-	databaseID, err := strconv.Atoi(c.Param("id"))
+func GetParamIDInt32(c *gin.Context) (int32, error) {
+	return GetParamInt32(c, "id")
+}
+
+func GetParamInt32(c *gin.Context, paramName string) (int32, error) {
+	databaseID, err := strconv.Atoi(c.Param(paramName))
 	if err != nil {
 		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": "an id is required to access the correct resource"})
 		return 0, err
