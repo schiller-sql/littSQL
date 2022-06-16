76afbdc848fcfe79ce2d28b64a56602ce6a7e7c0
bit-burger <65500763+bit-burger@users.noreply.github.com>
2022-06-16 13:27:57 +0200

    save login info locally with localStorage

--- frontend/src/auth.ts
+++ frontend/src/auth.ts
@@ -1,4 +1,5 @@
 import { writable, type Writable } from "svelte/store";
+import TeacherAuth from "./routes/auth/TeacherAuth.svelte";
 
 export enum UserType {
   teacher,
@@ -10,9 +11,62 @@ export interface User {
   type: UserType;
 }
 
+function userTypeFromString(rawUserType: string | null): UserType | null {
+  switch (rawUserType) {
+    case "teacher":
+      return UserType.teacher;
+    case "student":
+      return UserType.student;
+  }
+  return null;
+}
+
+function userTypeToString(userType: UserType): string {
+  switch (userType) {
+    case UserType.teacher:
+      return "teacher";
+    case UserType.student:
+      return "student";
+  }
+}
+
 export const DEFAULT_URL = "api/";
 
-export const authStore: Writable<User | null> = writable();
+const localStorageTokenKey = "token";
+const localStorageUserTypeKey = "user_type";
+
+function getUserFromLocalStorage(): User | null {
+  const token: string | null = localStorage.getItem(localStorageTokenKey);
+  const rawUserType: string | null = localStorage.getItem(
+    localStorageUserTypeKey
+  );
+  const userType: UserType | null = userTypeFromString(rawUserType);
+  if (token === null || userType === null) {
+    localStorage.clear();
+    return null;
+  }
+  return {
+    token,
+    type: userType,
+  };
+}
+
+function writeUserToLocalStorage(user: User) {
+  if (user === null) {
+    localStorage.clear();
+  } else {
+    localStorage.setItem(localStorageTokenKey, user.token);
+    localStorage.setItem(localStorageUserTypeKey, userTypeToString(user.type));
+  }
+}
+
+export const authStore: Writable<User | null> = writable(
+  getUserFromLocalStorage()
+);
+
+authStore.subscribe((user) => {
+  writeUserToLocalStorage(user);
+});
 
 export function fetchWithToken(url: string, method: string, token: string) {
   return requestWithToken(url, method, token).then((res) => res.json());
