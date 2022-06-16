bea4a2e55e753193334fdfafa6558d8f26291177
bit-burger <65500763+bit-burger@users.noreply.github.com>
2022-06-16 12:20:18 +0200

    implement deleting projects in Projects.svelte

--- frontend/src/auth.ts
+++ frontend/src/auth.ts
@@ -15,8 +15,12 @@ export const DEFAULT_URL = "api/";
 export const authStore: Writable<User | null> = writable();
 
 export function fetchWithToken(url: string, method: string, token: string) {
+  return requestWithToken(url, method, token).then((res) => res.json());
+}
+
+export function requestWithToken(url: string, method: string, token: string) {
   return fetch(DEFAULT_URL + url, {
     method: method,
-    headers: {Authorization: `Bearer ${token}`},
-  }).then((res) => res.json())
+    headers: { Authorization: `Bearer ${token}` },
+  });
 }
--- frontend/src/routes/teacher/Projects.svelte
+++ frontend/src/routes/teacher/Projects.svelte
@@ -1,37 +1,42 @@
 <script lang="ts">
-  import { onMount } from 'svelte'
-  import { authStore, fetchWithToken } from '../../auth'
-  import { Button, Loading, Modal, TextInput } from 'carbon-components-svelte'
-  import ProjectTile from '../../components/ProjectTile.svelte'
-  import type Project from '../../types/Project'
-  import Add from 'carbon-icons-svelte/lib/Add16/Add16.svelte'
+  import { onMount } from "svelte";
+  import { authStore, fetchWithToken, requestWithToken } from "../../auth";
+  import { Button, Loading, Modal, TextInput } from "carbon-components-svelte";
+  import ProjectTile from "../../components/ProjectTile.svelte";
+  import type Project from "../../types/Project";
+  import Add from "carbon-icons-svelte/lib/Add16/Add16.svelte";
 
   onMount(async () => {
     try {
-      projects = await fetchWithToken('projects', 'GET', $authStore.token)
-      loading = false
+      projects = await fetchWithToken("projects", "GET", $authStore.token);
+      loading = false;
     } catch (e) {
-      error = error.toString()
+      error = e.toString();
+      console.log(e);
     }
-  })
-  let open = false
-  let newProjectName: string
+  });
+  let open = false;
+  let newProjectName: string;
 
-  let loading = true
-  let error: string
-  let projects: Project[] = []
+  let loading = true;
+  let error: string;
+  let projects: Project[] = [];
 
-  //TODO: send request to the server
   async function deleteProject(event) {
-    const id = event.detail.id
+    const id = event.detail.id;
+    console.log(id);
     try {
-    } catch (e) {}
-    projects = projects.filter((project) => project.id != id)
+      await requestWithToken(`projects/${id}`, "DELETE", $authStore.token);
+      projects = projects.filter((project) => project.id != id);
+    } catch (e) {
+      console.log(e);
+      error = "could not delete project";
+    }
   }
 
   //TODO: navigation
   function openProject(event) {
-    const id = event.detail.id
+    const id = event.detail.id;
   }
 
   //TODO: send request to the server
@@ -56,18 +61,20 @@
       primaryButtonDisabled={!newProjectName}
       secondaryButtonText="Cancel"
       on:click:button--secondary={({ detail: { text } }) => {
-        if (text === 'Cancel') open = false
+        if (text === "Cancel") open = false;
       }}
-      on:open={() => (newProjectName = '')}
+      on:open={() => (newProjectName = "")}
       on:submit={() => {
-        open = false
-        addProject()
-      }}>
+        open = false;
+        addProject();
+      }}
+    >
       <TextInput
         bind:value={newProjectName}
         labelText="Project name"
         helperText="The project name required"
-        placeholder="Enter project name..." />
+        placeholder="Enter project name..."
+      />
     </Modal>
   {/if}
 </body>
