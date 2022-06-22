2cff7115d66a2a09e97b1698e7cf6ee0622c1679
leartpro <lenni.protte@gmx.de>
2022-06-22 21:59:49 +0200

    Fix ProjectTile.svelte

--- frontend/src/components/ProjectTile.svelte
+++ frontend/src/components/ProjectTile.svelte
@@ -1,51 +1,51 @@
 <script lang="ts">
-  import {
-    ClickableTile,
-    OverflowMenu,
-    OverflowMenuItem,
-    TooltipIcon,
-  } from "carbon-components-svelte";
-  import Edit16 from "carbon-icons-svelte/lib/Edit16/Edit16.svelte";
-  import View16 from "carbon-icons-svelte/lib/View16/View16.svelte";
-  import type ProjectListing from "../types/ProjectListing";
-  import { createEventDispatcher } from "svelte";
+    import {
+        ClickableTile,
+        OverflowMenu,
+        OverflowMenuItem,
+        TooltipIcon,
+    } from "carbon-components-svelte";
+    import Edit16 from "carbon-icons-svelte/lib/Edit16/Edit16.svelte";
+    import View16 from "carbon-icons-svelte/lib/View16/View16.svelte";
+    import type ProjectListing from "../types/ProjectListing";
+    import {createEventDispatcher} from "svelte";
 
-  export let project: ProjectListing;
+    export let project: ProjectListing;
 
-  const dispatch = createEventDispatcher();
+    const dispatch = createEventDispatcher();
 
-  function openProject() {
-    dispatch("open", { id: project.id });
-  }
-  function deleteProject() {
-    dispatch("delete", { id: project.id });
-  }
+    function openProject() {
+        dispatch("open", {id: project.id});
+    }
+
+    function deleteProject() {
+        dispatch("delete", {id: project.id});
+    }
 </script>
 
-<ClickableTile
-  style="display: grid; grid-template-columns: 1fr auto auto"
-  on:click={openProject}
->
-  {project.name}
-  {#if !project.is_public}
-    <TooltipIcon
-      tooltipText="private project"
-      on:click={openProject}
-      icon={Edit16}
-    />
-  {:else}
-    <TooltipIcon
-      tooltipText="public project"
-      on:click={openProject}
-      icon={View16}
-    />
-  {/if}
-  <OverflowMenu flipped>
-    {#if project.is_public}
-      <OverflowMenuItem on:click={openProject} text="View project" />
+<ClickableTile style="display: grid; grid-template-columns: 1fr auto auto">
+    <div on:click={openProject}>
+        {project.name}
+    </div>
+    {#if !project.is_public}
+        <TooltipIcon
+                tooltipText="private project"
+                on:click={openProject}
+                icon={Edit16}
+        />
     {:else}
-      <OverflowMenuItem on:click={openProject} text="Edit project" />
-      <OverflowMenuItem on:click={deleteProject} danger text="Delete project" />
+        <TooltipIcon
+                tooltipText="public project"
+                on:click={openProject}
+                icon={View16}
+        />
     {/if}
-  </OverflowMenu>
+    <OverflowMenu flipped style="z-index: 1">
+        {#if project.is_public}
+            <OverflowMenuItem on:click={openProject} text="View project"/>
+        {:else}
+            <OverflowMenuItem on:click={openProject} text="Edit project"/>
+            <OverflowMenuItem on:click={deleteProject} danger text="Delete project"/>
+        {/if}
+    </OverflowMenu>
 </ClickableTile>
