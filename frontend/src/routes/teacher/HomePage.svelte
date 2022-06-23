74b29d1aa8d189381556b37124d0709045e8bf7e
bit-burger <65500763+bit-burger@users.noreply.github.com>
2022-06-23 16:40:50 +0200

    save last used tab in homepage selected

--- frontend/src/routes/teacher/HomePage.svelte
+++ frontend/src/routes/teacher/HomePage.svelte
@@ -1,16 +1,31 @@
 <script lang="ts">
-  import { Tabs, Tab, TabContent } from 'carbon-components-svelte'
-  import Courses from './Courses.svelte'
-  import Projects from './Projects.svelte'
+  import { Tabs, Tab, TabContent } from "carbon-components-svelte";
+  import { onMount } from "svelte";
+  import Courses from "./Courses.svelte";
+  import Projects from "./Projects.svelte";
+
+  const lastSelectedTabKey = "home_page_last_selected_tab";
+
+  onMount(() => {
+    const rawLastSelectedTab = localStorage.getItem(lastSelectedTabKey);
+    if (rawLastSelectedTab !== null) {
+      selected = Number.parseInt(rawLastSelectedTab);
+    } else {
+      selected = 0;
+    }
+  });
+
+  let selected: number;
+  $: localStorage.setItem(lastSelectedTabKey, selected.toString());
 </script>
 
 <p>Logged in as a teacher!</p>
 
-<Tabs>
+<Tabs bind:selected>
   <Tab label="Courses" />
   <Tab label="Projects" />
   <svelte:fragment slot="content">
-    <TabContent> <Courses /> </TabContent>
-    <TabContent> <Projects /> </TabContent>
+    <TabContent><Courses /></TabContent>
+    <TabContent><Projects /></TabContent>
   </svelte:fragment>
 </Tabs>
