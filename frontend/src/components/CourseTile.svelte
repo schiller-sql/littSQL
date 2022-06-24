8d71316e9ae6ca0941f67f909d003c71fd80a918
bit-burger <65500763+bit-burger@users.noreply.github.com>
2022-06-24 09:56:18 +0200

    fix CourseTile and ProjectTile

--- frontend/src/components/CourseTile.svelte
+++ frontend/src/components/CourseTile.svelte
@@ -1,28 +1,33 @@
 <script lang="ts">
-    import {
-        ClickableTile,
-        TooltipIcon,
-    } from "carbon-components-svelte";
-    import Edit16 from "carbon-icons-svelte/lib/Edit16/Edit16.svelte";
-    import type ProjectListing from "../types/ProjectListing";
-    import {createEventDispatcher} from "svelte";
-    import {TrashCan16} from "carbon-icons-svelte";
+  import { ClickableTile, TooltipIcon } from "carbon-components-svelte";
+  import Edit16 from "carbon-icons-svelte/lib/Edit16/Edit16.svelte";
+  import { createEventDispatcher } from "svelte";
+  import { TrashCan16 } from "carbon-icons-svelte";
+  import type CourseListing from "../types/CourseListing";
 
-    export let course: ProjectListing;
+  export let course: CourseListing;
 
-    const dispatch = createEventDispatcher();
+  const dispatch = createEventDispatcher();
 
-    function openCourse() {
-        dispatch("open", {id: course.id});
-    }
+  function openCourse() {
+    dispatch("open", { id: course.id });
+  }
 
-    function deleteCourse() {
-        dispatch("delete", {id: course.id});
-    }
+  function deleteCourse() {
+    dispatch("delete", { id: course.id });
+  }
 </script>
 
-<ClickableTile on:click={openCourse} style="display: grid; grid-template-columns: 1fr auto auto">
-        {course.name}
-    <TooltipIcon tooltipTest="edit course" icon={Edit16} />
-    <TooltipIcon tooltipTest="delete course" icon={TrashCan16} on:click={deleteCourse} style="margin-left: 0.66rem"/>
+<ClickableTile
+  on:click={openCourse}
+  style="display: grid; grid-template-columns: 1fr auto auto"
+>
+  {course.name}
+  <TooltipIcon tooltipText="edit course" icon={Edit16} />
+  <TooltipIcon
+    tooltipText="delete course"
+    icon={TrashCan16}
+    on:click={deleteCourse}
+    style="margin-left: 0.66rem"
+  />
 </ClickableTile>
--- frontend/src/components/ProjectTile.svelte
+++ frontend/src/components/ProjectTile.svelte
@@ -1,47 +1,52 @@
 <script lang="ts">
-    import {
-        ClickableTile,
-        OverflowMenu,
-        OverflowMenuItem,
-        TooltipIcon,
-    } from "carbon-components-svelte";
-    import Edit16 from "carbon-icons-svelte/lib/Edit16/Edit16.svelte";
-    import View16 from "carbon-icons-svelte/lib/View16/View16.svelte";
-    import type ProjectListing from "../types/ProjectListing";
-    import {createEventDispatcher} from "svelte";
-    import {TrashCan16} from "carbon-icons-svelte";
+  import {
+    ClickableTile,
+    OverflowMenu,
+    OverflowMenuItem,
+    TooltipIcon,
+  } from "carbon-components-svelte";
+  import Edit16 from "carbon-icons-svelte/lib/Edit16/Edit16.svelte";
+  import View16 from "carbon-icons-svelte/lib/View16/View16.svelte";
+  import type ProjectListing from "../types/ProjectListing";
+  import { createEventDispatcher } from "svelte";
+  import { TrashCan16 } from "carbon-icons-svelte";
 
-    export let project: ProjectListing;
+  export let project: ProjectListing;
 
-    const dispatch = createEventDispatcher();
+  const dispatch = createEventDispatcher();
 
-    function openProject() {
-        dispatch("open", {id: project.id});
-    }
+  function openProject() {
+    dispatch("open", { id: project.id });
+  }
 
-    function deleteProject() {
-        dispatch("delete", {id: project.id});
-    }
+  function deleteProject() {
+    dispatch("delete", { id: project.id });
+  }
 </script>
 
 <ClickableTile style="display: grid; grid-template-columns: 1fr auto auto">
-    <div on:click={openProject}>
-        {project.name}
-    </div>
-    {#if !project.is_public}
-        <TooltipIcon
-                tooltipText="private project"
-                on:click={openProject}
-                icon={Edit16}
-        />
-    {:else}
-        <TooltipIcon
-                tooltipText="public project"
-                on:click={openProject}
-                icon={View16}
-        />
-    {/if}
-        {#if !project.is_public}
-            <TooltipIcon tooltipTest="delete project" icon={TrashCan16} on:click={deleteProject} style="margin-left: 0.66rem"/>
-        {/if}
+  <div on:click={openProject}>
+    {project.name}
+  </div>
+  {#if !project.is_public}
+    <TooltipIcon
+      tooltipText="private project"
+      on:click={openProject}
+      icon={Edit16}
+    />
+  {:else}
+    <TooltipIcon
+      tooltipText="public project"
+      on:click={openProject}
+      icon={View16}
+    />
+  {/if}
+  {#if !project.is_public}
+    <TooltipIcon
+      tooltipText="delete project"
+      icon={TrashCan16}
+      on:click={deleteProject}
+      style="margin-left: 0.66rem"
+    />
+  {/if}
 </ClickableTile>
