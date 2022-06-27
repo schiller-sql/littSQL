d68a8b10865174747d2721669be409ea394ff90d
bit-burger <65500763+bit-burger@users.noreply.github.com>
2022-06-27 21:23:55 +0200

    improve letterFromNumber function to not only be valid until 25

--- frontend/src/util/utli.ts
+++ frontend/src/util/utli.ts
@@ -1,5 +1,20 @@
 const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-export function letterFromNumber(n) {
+function basicLetterFromNumber(n) {
   return alphabet[n];
+}
+
+export function letterFromNumber(n) {
+  if (n === 0) return "a";
+  let name = "";
+  while (n >= 0) {
+    if (n >= alphabet.length) {
+      n -= alphabet.length - 1;
+      name += "Z";
+    } else if (n !== 0) {
+      name += basicLetterFromNumber(n);
+      break;
+    }
+  }
+  return name;
 }
