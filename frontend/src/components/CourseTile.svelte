ba8446f0031f0f718000825e4d8167a0110c5fc5
Tony <65500763+bit-burger@users.noreply.github.com>
2022-07-13 22:42:07 -0700

    improve ProjectTile and CourseTile click layout

--- frontend/src/components/CourseTile.svelte
+++ frontend/src/components/CourseTile.svelte
@@ -19,10 +19,11 @@
 </script>
 
 <ClickableTile
-  on:click={openCourse}
-  style="display: grid; grid-template-columns: 1fr auto auto"
+  style="display: grid; grid-template-columns: 1fr auto auto; padding: 0; padding-right: 16px"
 >
-  {course.name}
+  <div on:click={openCourse} style="padding: 16px">
+    {course.name}
+  </div>
   <TooltipIcon tooltipText="edit course" icon={Edit16} />
   <TooltipIcon
     tooltipText="delete course"
--- frontend/src/components/ProjectTile.svelte
+++ frontend/src/components/ProjectTile.svelte
@@ -1,10 +1,5 @@
 <script lang="ts">
-  import {
-    ClickableTile,
-    OverflowMenu,
-    OverflowMenuItem,
-    TooltipIcon,
-  } from "carbon-components-svelte";
+  import { ClickableTile, TooltipIcon } from "carbon-components-svelte";
   import Edit16 from "carbon-icons-svelte/lib/Edit16/Edit16.svelte";
   import View16 from "carbon-icons-svelte/lib/View16/View16.svelte";
   import type ProjectListing from "../types/ProjectListing";
@@ -24,8 +19,10 @@
   }
 </script>
 
-<ClickableTile style="display: grid; grid-template-columns: 1fr auto auto">
-  <div on:click={openProject}>
+<ClickableTile
+  style="display: grid; grid-template-columns: 1fr auto auto; padding: 0; padding-right: 16px"
+>
+  <div on:click={openProject} style="padding: 16px">
     {project.name}
   </div>
   {#if !project.is_public}
