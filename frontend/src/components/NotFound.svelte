3bc72a6df7849018f143745b6bca7704f086051d
bit-burger <65500763+bit-burger@users.noreply.github.com>
2022-08-06 00:53:24 +0200

    improve design of 404 page

--- frontend/src/components/NotFound.svelte
+++ frontend/src/components/NotFound.svelte
@@ -2,4 +2,17 @@
   export let params;
 </script>
 
-<p>404: Could not find {params.wild}</p>
+<h3><span class="highlighted">404: </span>Could not find '{params.wild}'</h3>
+
+<br />
+
+<p>
+  Please make sure to login in as teacher or student to view their respective
+  pages.
+</p>
+
+<style>
+  span.highlighted {
+    font-weight: 800;
+  }
+</style>
